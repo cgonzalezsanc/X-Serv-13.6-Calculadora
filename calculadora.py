c1071c0419ec7195eba73943bdb49cace469a761
carlos gonzalez sanchez <sanchezc@lambda14.aulas.gsyc.es>
2015-02-03 10:52:37 +0100

    wgjm

---
+++ calculadora.py
@@ -0,0 +1,22 @@
+#!/bin/usr/python
+# -*- coding: utf-8 -*-
+
+"""
+Script para multiplicar, dividir, sumar o restar dos números.
+
+Para ejecutarlo, desde la shell:
+$ python calculadora.py funcion operando1 operando2
+
+"""
+
+import sys
+
+if len(sys.argv) != 3:
+    print
+    sys.exit("Usage: $ python calculadora.py funcion operando1 operando2")
+
+
+if __name__ == "__main__":
+
+
+

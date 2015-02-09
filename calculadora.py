57a3d760e9ed68fef5e8e80da5d9c13f7ecde04e
Carlos <carlos@carlos-laptop.(none)>
2015-02-09 21:53:51 +0100

    Ejercicio 13.6

--- calculadora.py
+++ calculadora.py
@@ -12,44 +12,49 @@ $ python calculadora.py funcion operando1 operando2
 import sys
 import os
 
-def suma(sumando1, sumando2):
-	"""Suma de dos enteros/reales"""
 
-	return sumando1 + sumando2
+def suma(sumando1, sumando2):
+    """Suma de dos enteros/reales"""
+
+    return sumando1 + sumando2
+
 
 def resta(minuendo, sustraendo):
-	"""Resta de dos enteros/reales"""
+    """Resta de dos enteros/reales"""
 
-	return minuendo - sustraendo
+    return minuendo - sustraendo
 
 
 def multi(factor1, factor2):
-	"""Multiplicacion de dos enteros/reales"""
+    """Multiplicacion de dos enteros/reales"""
 
-	return factor1 * factor2
+    return factor1 * factor2
+
 
 def division(dividendo, divisor):
-	"""Division de dos enteros/reales"""
+    """Division de dos enteros/reales"""
 
-	return dividendo / divisor
+    return dividendo / divisor
 
 
 if __name__ == "__main__":
 
-	if len(sys.argv) != 4:
-   		sys.exit("\nUsage: $ python calculadora.py funcion operando1 operando2")
+    if len(sys.argv) != 4:
+        sys.exit("\nUsage: $ python calculadora.py" +
+                " funcion operando1 operando2")
 
-	funcion = sys.argv[1]
-	operando1 = int(sys.argv[2])
-	operando2 = int(sys.argv[3])
-	if funcion == "sumar":
-		resultado = suma(operando1,operando2)
-	elif funcion == "restar":
-		resultado = resta(operando1,operando2)
-	elif funcion == "multiplicar":
-		resultado = multi(operando1,operando2)
-	elif funcion == "dividir":
-		resultado = division(operando1,operando2)
-	else:
-		sys.exit("\nLas operaciones validas son: sumar, restar, multiplicar y dividir")
-	print "\nEl resultado es " + str(resultado)
+    funcion = sys.argv[1]
+    operando1 = int(sys.argv[2])
+    operando2 = int(sys.argv[3])
+    if funcion == "sumar":
+        resultado = suma(operando1,	operando2)
+    elif funcion == "restar":
+        resultado = resta(operando1, operando2)
+    elif funcion == "multiplicar":
+        resultado = multi(operando1, operando2)
+    elif funcion == "dividir":
+        resultado = division(operando1, operando2)
+    else:
+        sys.exit("\nLas operaciones validas son:" +
+                " sumar, restar, multiplicar y dividir")
+    print "\nEl resultado es " + str(resultado)
